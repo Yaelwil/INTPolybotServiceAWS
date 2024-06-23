583b6a9e3ccca428c13ecdb7e69d1bb64eb29f47
YaelWil <yaelwilamo@gmail.com>
2024-06-23 17:35:44 +0300

    commit 23.06.2024 17:35

--- filters/app.py
+++ filters/app.py
@@ -1,3 +1,71 @@
+import time
+from pathlib import Path
+from detect import run  # Ensure detect.py is in your working directory and contains the run function
+import yaml
+from loguru import logger
+import os
+import boto3
+import requests
+import json
+import uuid
+from img_proc import Img
+from decimal import Decimal, ROUND_HALF_UP
+
+# Load environment variables
+images_bucket = os.environ["BUCKET_NAME"]
+queue_name = os.environ["YOLO_QUEUE_NAME"]
+region = os.environ["REGION"]
+dynamodb_table_name = os.environ["DYNAMODB_TABLE_NAME"]
+alb_url = os.environ["ALB_URL"]
+
+# Initialize SQS client, bucket and DynamoDB table
+sqs_client = boto3.client('sqs', region_name=region)
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb', region_name=region)
+dynamodb_table = dynamodb.Table(dynamodb_table_name)
+
+# Load class names from coco128.yaml
+with open("data/coco128.yaml", "r") as stream:
+    names = yaml.safe_load(stream)['names']
+
+def consume():
+    while True:
+        response = sqs_client.receive_message(QueueUrl=queue_name, MaxNumberOfMessages=1, WaitTimeSeconds=5)
+
+        if 'Messages' in response:
+            message_body = response['Messages'][0]['Body']
+            receipt_handle = response['Messages'][0]['ReceiptHandle']
+            prediction_id = response['Messages'][0]['MessageId']
+
+            logger.info(f'Raw message body: {message_body}')
+
+            try:
+                # Split the message body by commas and extract the relevant fields
+                chat_id, predict, full_s3_path, img_name = message_body.split(', ')
+            except ValueError as e:
+                logger.error(f"Error parsing message body: {e}")
+                sqs_client.delete_message(QueueUrl=queue_name, ReceiptHandle=receipt_handle)
+                continue
+
+            logger.info(f'Prediction: {prediction_id}. Start processing')
+
+            if not img_name or not chat_id or not full_s3_path:
+                logger.error('Invalid message format: chat_id, file_name, or s3_key missing')
+                sqs_client.delete_message(QueueUrl=queue_name, ReceiptHandle=receipt_handle)
+                continue
+
+            logger.info(f'img_name received: {img_name}')
+            s3.download_file(images_bucket, full_s3_path, img_name)
+            original_img_path = img_name
+
+
+
+
+
+
+
+
+
 from img_proc import Img
 
 class Filters:
--- yolo5/app.py
+++ yolo5/app.py
@@ -15,7 +15,7 @@ images_bucket = os.environ["BUCKET_NAME"]
 queue_name = os.environ["YOLO_QUEUE_NAME"]
 region = os.environ["REGION"]
 dynamodb_table_name = os.environ["DYNAMODB_TABLE_NAME"]
-polybot_url = os.environ["POLYBOT_URL"]
+alb_url = os.environ["ALB_URL"]
 
 # Initialize SQS client, bucket and DynamoDB table
 sqs_client = boto3.client('sqs', region_name=region)
@@ -54,8 +54,7 @@ def consume():
                 continue
 
             logger.info(f'img_name received: {img_name}')
-            s3_client = boto3.client('s3')
-            s3_client.download_file(images_bucket, full_s3_path, img_name)
+            s3.download_file(images_bucket, full_s3_path, img_name)
             original_img_path = img_name
             logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
@@ -126,16 +125,15 @@ def consume():
                 s3.upload_file(json_file_path, images_bucket, json_full_path)
                 logger.info('Upload successfully the results to S3')
 
-                # Store the prediction summary in DynamoDB with chat_id as primary key
-                dynamodb_table.put_item(Item={
-                    'chat_id': chat_id,
-                    'prediction_summary': prediction_summary
-                })
-                logger.info(f'Stored prediction summary for chat_id {chat_id} in DynamoDB')
+                # TODO store the prediction_summary in a DynamoDB table
 
+                # TODO perform a GET request to Polybot to `/results` endpoint
+                endpoint_path = '/results'
+                url = alb_url + endpoint_path
 
             # Delete the message from the queue after processing
             sqs_client.delete_message(QueueUrl=queue_name, ReceiptHandle=receipt_handle)
+
 
 if __name__ == "__main__":
     consume()
