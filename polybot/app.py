4eed609ace980b8dbd57080a28a9473708281b1f
YaelWil <yaelwilamo@gmail.com>
2024-06-30 21:46:54 +0300

    commit 30.06.2024 21:45

--- polybot/app.py
+++ polybot/app.py
@@ -29,6 +29,7 @@ REGION = os.environ["REGION"]
 DYNAMODB_TABLE_NAME = os.environ["DYNAMODB_TABLE_NAME"]
 BUCKET_NAME = os.environ["BUCKET_NAME"]
 alb_url = os.environ["ALB_URL"]
+
 print(f"TELEGRAM_APP_URL: {TELEGRAM_APP_URL}")
 
 domain_certificate_file = 'DOMAIN_CERTIFICATE.pem'
