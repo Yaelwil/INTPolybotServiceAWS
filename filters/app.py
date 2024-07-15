5fcb9ab96091331d2531f53bc336294444e8d93a
YaelWil <yaelwilamo@gmail.com>
2024-07-15 17:27:03 +0300

    commit 15.07.2024 17:27

--- filters/app.py
+++ filters/app.py
@@ -10,6 +10,7 @@ queue_name = os.environ["FILTERS_QUEUE_NAME"]
 region = os.environ["REGION"]
 alb_url = os.environ["ALB_URL"]
 
+
 # Initialize SQS client, S3 client resources
 sqs_client = boto3.client('sqs', region_name=region)
 s3 = boto3.client('s3')
