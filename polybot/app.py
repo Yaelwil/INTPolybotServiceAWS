6683fb49e5a8d8739475b84ad911181246fda252
YaelWil <yaelwilamo@gmail.com>
2024-07-19 17:17:29 +0300

    commit 19.07.2024 17:17

--- polybot/app.py
+++ polybot/app.py
@@ -12,7 +12,7 @@ from botocore.exceptions import NoCredentialsError, PartialCredentialsError, NoR
 app = flask.Flask(__name__)
 
 # Load TELEGRAM_TOKEN value from Secret Manager
-secret_name_TELEGRAM_TOKEN = "TELEGRAM_TOKEN"
+secret_name_TELEGRAM_TOKEN = "yaelwil-token-tf-project"
 secret_name_DOMAIN_CERTIFICATE = "yaelwil-certificate-tf-project"
 secret_value_TELEGRAM_TOKEN = get_secret(secret_name_TELEGRAM_TOKEN)
 DOMAIN_CERTIFICATE = get_secret(secret_name_DOMAIN_CERTIFICATE)
