3fc3562a1c29b4215198c3384c62dee241bd7823
YaelWil <yaelwilamo@gmail.com>
2024-07-25 19:03:56 +0300

    commit 25.07.2024 18:53

--- polybot/app.py
+++ polybot/app.py
@@ -9,6 +9,7 @@ import results
 
 app = flask.Flask(__name__)
 
+
 # Load TELEGRAM_TOKEN value from Secret Manager
 secret_name_DOMAIN_CERTIFICATE = "yaelwil-mn-certificate-tf-project"
 DOMAIN_CERTIFICATE = get_secret(secret_name_DOMAIN_CERTIFICATE)
