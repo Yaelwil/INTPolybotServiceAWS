0027b4b1b91563b8a539804f921c74472d676afc
YaelWil <yaelwilamo@gmail.com>
2024-07-26 10:20:45 +0300

    commit 26.07.2024 09:55

--- terraform/modules/permissions/iam_policies.tf
+++ terraform/modules/permissions/iam_policies.tf
@@ -101,7 +101,8 @@ resource "aws_iam_policy" "secrets_manager_access_policy" {
       {
         Effect   = "Allow",
         Action   = [
-          "secretsmanager:GetSecretValue"
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:ListSecrets"
         ],
         Resource = "*"
       }
