fbf887279eab13d9812ca3896845fcdf87940013
YaelWil <yaelwilamo@gmail.com>
2024-07-26 14:06:51 +0300

    commit 26.07.2024 09:55

--- polybot/app.py
+++ polybot/app.py
@@ -17,22 +17,12 @@ BUCKET_NAME = os.environ["BUCKET_NAME"]
 alb_url = os.environ["ALB_URL"]
 print(f"TELEGRAM_APP_URL: {TELEGRAM_APP_URL}")
 
-
-# # Load TELEGRAM_TOKEN value from Secret Manager
-# secret_name_DOMAIN_CERTIFICATE = "yaelwil-mn-certificate-tf-project"
-# DOMAIN_CERTIFICATE = get_secret(secret_name_DOMAIN_CERTIFICATE)
-#
-# if DOMAIN_CERTIFICATE:
-#     logger.info('Retrieved DOMAIN_CERTIFICATE from Secrets Manager')
-# else:
-#     raise ValueError("Failed to retrieve secret TELEGRAM_TOKEN and DOMAIN_CERTIFICATE from Secrets Manager")
-
 prefix = "yaelwil-certificate"
-secret_name_1 = "TELEGRAM_TOKEN_EU_WEST_1"
-secret_name_2 = "TELEGRAM_TOKEN_US_EAST_1"
+# secret_name_1 = "TELEGRAM_TOKEN_EU_WEST_1"
+# secret_name_2 = "TELEGRAM_TOKEN_US_EAST_1"
 DOMAIN_CERTIFICATE = get_cert(prefix)
-TELEGRAM_TOKEN_EU_WEST_1 = get_secret(secret_name_1)
-TELEGRAM_TOKEN_US_EAST_1 = get_secret(secret_name_2)
+# TELEGRAM_TOKEN_EU_WEST_1 = get_secret(secret_name_1)
+# TELEGRAM_TOKEN_US_EAST_1 = get_secret(secret_name_2)
 
 if DOMAIN_CERTIFICATE:
     logger.info('Retrieved DOMAIN_CERTIFICATE from Secrets Manager')
@@ -46,11 +36,11 @@ with open(domain_certificate_file, 'w') as file:
 
 logger.info('Created certificate file successfully')
 
-# Make sure to use the correct variable
-region_upper = REGION.upper()
-# Example of constructing variable name dynamically (Note: this may need adjustment based on how tokens are handled)
-telegram_token_env_var = f"TELEGRAM_TOKEN_{region_upper}"
-TELEGRAM_TOKEN = os.environ.get(telegram_token_env_var)
+# # Make sure to use the correct variable
+# region_upper = REGION.upper()
+# # Example of constructing variable name dynamically (Note: this may need adjustment based on how tokens are handled)
+# telegram_token_env_var = f"TELEGRAM_TOKEN_{region_upper}"
+# TELEGRAM_TOKEN = os.environ.get(telegram_token_env_var)
 
 # Initialize bot outside of __main__ block
 bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL, domain_certificate_file)
