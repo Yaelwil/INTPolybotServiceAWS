90e18cda0a5e1883fae71a715aeb71b3caa091ed
YaelWil <yaelwilamo@gmail.com>
2024-07-27 10:42:10 +0300

    commit 27.07.2024 10:30

--- terraform/modules/permissions/iam_policies.tf
+++ terraform/modules/permissions/iam_policies.tf
@@ -13,7 +13,8 @@ resource "aws_iam_policy" "s3_access_policy" {
             "Action": [
                 "s3:PutObject",
                 "s3:GetObject",
-                "s3:ListBucket"
+                "s3:ListBucket",
+                "s3:DeleteObject"
             ],
             "Resource": [
                 var.bucket_arn,
