682c7fab49e74094899696cfa7440740c4792e7a
YaelWil <yaelwilamo@gmail.com>
2024-07-29 19:25:57 +0300

    commit 27.07.2024 10:30

--- terraform/modules/resources/ssh_key.tf
+++ terraform/modules/resources/ssh_key.tf
@@ -1,12 +1,21 @@
-resource "tls_private_key" "aws_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
+# resource "tls_private_key" "aws_key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 2048
+# }
+#
+#
+# resource "aws_key_pair" "my_key_pair" {
+#   key_name   = "yaelwil-tf"
+#   public_key = tls_private_key.aws_key.public_key_openssh
+#
+#   tags = {
+#     Terraform   = "true"
+#   }
+# }
 
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "yaelwil-tf"
-  public_key = tls_private_key.aws_key.public_key_openssh
+  public_key = file(var.public_key_path)
 
   tags = {
     Terraform   = "true"
